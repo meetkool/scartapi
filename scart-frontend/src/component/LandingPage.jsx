faa1f910b67f9c0cfda96b1aab4f7b6e652c77ec
meet bhanushali <lcs2021023@iiitl.ac.in>
2023-07-19 18:04:55 +0530

    sdkjfh

--- scart-frontend/src/component/LandingPage.jsx
+++ scart-frontend/src/component/LandingPage.jsx
@@ -15,71 +15,81 @@ function LandingPage() {
     try {
       let response;
       if (searchQuery) {
-        response = await axios.get("http://127.0.0.1:5000/products/search", { params: { query: searchQuery } });
+        response = await axios.get("https://scart-xebia.onrender.com/products/search", { params: { query: searchQuery } });
+      } else if (Object.keys(filter).length !== 0) {
+        response = await axios.get("https://scart-xebia.onrender.com/filter", { params: { min_price: priceRange.min, max_price: priceRange.max, min_discount: discountRange.min, max_discount: discountRange.max } });
       } else {
-        response = await axios.get("http://127.0.0.1:5000/products");
+        response = await axios.get("https://scart-xebia.onrender.com/products");
       }
       setProducts(response.data.products);
-    const uniqueBrands = [...new Set(response.data.products.map(product => product.brand))];
-    setBrands(uniqueBrands);
-    const uniqueColors = [...new Set(response.data.products.map(product => product.color))];
-    setColors(uniqueColors);
-    const maxPrice = Math.max(...response.data.products.map(product => product.price));
-    const minPrice = Math.min(...response.data.products.map(product => product.price));
-    setPriceRange({ min: minPrice, max: maxPrice });
-    const maxDiscount = Math.max(...response.data.products.map(product => product.discount));
-    const minDiscount = Math.min(...response.data.products.map(product => product.discount));
-    setDiscountRange({ min: minDiscount, max: maxDiscount });
-
-  } catch (error) {
-    console.log(error);
-    if (error.response && error.response.status === 404) {
-      setProducts([]);
+      const uniqueBrands = [...new Set(response.data.products.map(product => product.brand))];
+      setBrands(uniqueBrands);
+      const uniqueColors = [...new Set(response.data.products.map(product => product.color))];
+      setColors(uniqueColors);
+      const maxPrice = Math.max(...response.data.products.map(product => product.price));
+      const minPrice = Math.min(...response.data.products.map(product => product.price));
+      setPriceRange({ min: minPrice, max: maxPrice });
+      const maxDiscount = Math.max(...response.data.products.map(product => product.discount));
+      const minDiscount = Math.min(...response.data.products.map(product => product.discount));
+      setDiscountRange({ min: minDiscount, max: maxDiscount });
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 404) {
+        setProducts([]);
+      }
     }
-  }
-}, [searchQuery]);
+  }, [searchQuery, filter, priceRange.min, priceRange.max, discountRange.min, discountRange.max]);
+  
+  
 
 useEffect(() => {
   fetchProducts();
 }, [fetchProducts]);
 
-  const handleFilterChange = (e) => {
-    const { name, value, type } = e.target;
-    setFilter((prevFilter) => {
-      let updatedFilter;
-      if (type === 'checkbox') {
-        if (prevFilter[name] && prevFilter[name].length) {
-          if (prevFilter[name].includes(value)) {
+const handleFilterChange = (e) => {
+  const { name, value, type } = e.target;
+  setFilter((prevFilter) => {
+    let updatedFilter;
+    if (type === 'checkbox') {
+      if (prevFilter[name] && prevFilter[name].length) {
+        if (prevFilter[name].includes(value)) {
+          const newFilterValues = prevFilter[name].filter((v) => v !== value);
+          if (newFilterValues.length > 0) {
             updatedFilter = {
               ...prevFilter,
-              [name]: prevFilter[name].filter((v) => v !== value),
+              [name]: newFilterValues,
             };
           } else {
-            updatedFilter = {
-              ...prevFilter,
-              [name]: [...prevFilter[name], value],
-            };
+            const { [name]: deletedKey, ...remaining } = prevFilter;
+            updatedFilter = remaining;
           }
         } else {
           updatedFilter = {
             ...prevFilter,
-            [name]: [value],
+            [name]: [...prevFilter[name], value],
           };
         }
-      } else if (type === 'number') {
-        updatedFilter = {
-          ...prevFilter,
-          [name]: parseFloat(value),
-        };
       } else {
         updatedFilter = {
           ...prevFilter,
-          [name]: value,
+          [name]: [value],
         };
       }
-      return updatedFilter;
-    });
-  };
+    } else if (type === 'number') {
+      updatedFilter = {
+        ...prevFilter,
+        [name]: parseFloat(value),
+      };
+    } else {
+      updatedFilter = {
+        ...prevFilter,
+        [name]: value,
+      };
+    }
+    return updatedFilter;
+  });
+};
+
 
   const handleResetFilters = () => {
     setFilter({});
