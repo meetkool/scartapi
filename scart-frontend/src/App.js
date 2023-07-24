86bfc6e75898c278fcf7d24a3b92c4e8f055d1a3
Sonal Sangvi <Sonal.Sangvi@XEBIAINDIA.LOCAL>
2023-07-24 13:20:28 +0530

    header

--- scart-frontend/src/App.js
+++ scart-frontend/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './component/LandingPage';
 import ProductDetail from './component/ProductDetail';
+import Header from './component/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
+    <Header />
       <Routes>
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/" element={<LandingPage />} />
